d95a92369f109a2a4c520520a8cc296a9841f8a2
Sergey Hovhannisyan <sergey.hovhannisyan.1992@gmail.com>
2023-10-24 12:40:25 -0700

    controllerChanges

--- ProgrammingClass4.AngularLesson/Controllers/ProductTypesController.cs
+++ ProgrammingClass4.AngularLesson/Controllers/ProductTypesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProgrammingClass4.AngularLesson.Data;
+using ProgrammingClass4.AngularLesson.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ProgrammingClass4.AngularLesson.Controllers
 {
@@ -13,6 +15,72 @@ namespace ProgrammingClass4.AngularLesson.Controllers
         public ProductTypesController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllProductTypes()
+        {
+            var productTypes = _dbContext.ProductTypes.ToList();
+
+            return Ok(productTypes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductType(int id)
+        {
+            var productType = _dbContext.ProductTypes.Find(id);
+
+            if (productType != null)
+            {
+                return Ok(productType);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AddProductType(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Add(productType);
+                _dbContext.SaveChanges();
+
+                return Ok(productType);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProductType(int id, ProductType productType)
+        {
+            if (productType.Id != id)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Update(productType);
+                _dbContext.SaveChanges();
+
+                return Ok(productType);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProductType(int id)
+        {
+            var productType = _dbContext.ProductTypes.Find(id);
+
+            if (productType != null)
+            {
+                _dbContext.ProductTypes.Remove(productType);
+                _dbContext.SaveChanges();
+
+                return Ok(productType);
+            }
+
+            return NotFound();
         }
     }
 }
--- ProgrammingClass4.AngularLesson/Controllers/UnitOfMeasuresController.cs
+++ ProgrammingClass4.AngularLesson/Controllers/UnitOfMeasuresController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProgrammingClass4.AngularLesson.Data;
+using ProgrammingClass4.AngularLesson.Models;
 
 namespace ProgrammingClass4.AngularLesson.Controllers
 {
@@ -13,6 +14,73 @@ namespace ProgrammingClass4.AngularLesson.Controllers
         public UnitOfMeasuresController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUnitOfMeasures()
+        {
+            var unitOfMeasures = _dbContext.UnitOfMeasures.ToList();
+
+            return Ok(unitOfMeasures);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetUnitOfMeasure(int id)
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.Find(id);
+
+            if (unitOfMeasure != null)
+            {
+                return Ok(unitOfMeasure);
+            }
+
+            return NotFound();
+        }
+        [HttpPost]
+        public IActionResult AddUnitOfMeasure(UnitOfMeasure unitOfMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Add(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return Ok(unitOfMeasure);
+            }
+
+            return BadRequest(ModelState);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateUnitOfMeasure(int id, UnitOfMeasure unitOfMeasure)
+        {
+            if (id != unitOfMeasure.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Update(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return Ok(unitOfMeasure);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUnitOfMeasure(int id)
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.Find(id);
+
+            if (unitOfMeasure != null)
+            {
+                _dbContext.UnitOfMeasures.Remove(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return Ok(unitOfMeasure);
+            }
+
+            return NotFound();
         }
     }
 }
